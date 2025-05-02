11bc2270cc84a507aff90fdd687e97f7654dbddb
Alex Wendland <a.p.wendland@gmail.com>
2025-05-02 17:17:11 +0100

    Update render to include publishing backend

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,12 +1,31 @@
+'use client';
+
+import { useEffect, useState } from "react";
 import Link from "next/link";
 
 export default function Home() {
+  const [message, setMessage] = useState("Loading...");
+  console.log("Backend URL:", process.env.NEXT_PUBLIC_BACKEND_URL);
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message) {
+          setMessage(data.message);
+        } else {
+          setMessage("No message field in response");
+        }
+      })
+      .catch((err) => {
+        console.error("Failed to fetch:", err);
+        setMessage("Failed to connect to backend");
+      });
+  }, []);
+
   return (
     <div>
-      Hello World Alex.{" "}
-      <Link href="/about">
-        About
-      </Link>
+      <p>Backend says: {message}</p>
+      <Link href="/about">About</Link>
     </div>
   );
 }
