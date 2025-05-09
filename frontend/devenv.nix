4e25b0f849516663b8841ea22733afd1145e7a17
Alex Wendland <a.p.wendland@gmail.com>
2025-05-09 22:31:11 +0100

    Update build command

--- frontend/devenv.nix
+++ frontend/devenv.nix
@@ -15,6 +15,6 @@
     docker build -t games-frontend .
   '';
   scripts.docker-up.exec = ''
-    docker run --env-file .env.local -p 3000:10000 games-frontend
+    docker run --env-file .env.local -p 3000:3000 games-frontend
   '';
 }
