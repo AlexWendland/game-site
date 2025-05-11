9b81ae6901a12afe8c08ac3f8b9737da9d6f7433
Alex Wendland <a.p.wendland@gmail.com>
2025-05-11 13:17:13 +0100

    Fix CORS issue

--- backend/games_backend/games/__init__.py
+++
@@ -1,2 +0,0 @@
-from .tictactoe import TicTacToeGame  # noqa: F401
-from .ultimate import UltimateGame  # noqa: F401
--- backend/games_backend/main.py
+++ backend/games_backend/main.py
@@ -8,7 +8,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from games_backend import models, utils
 from games_backend.app_logger import logger
-from games_backend.games import TicTacToeGame, UltimateGame
+from games_backend.games.tictactoe import TicTacToeGame
+from games_backend.games.ultimate import UltimateGame
 from games_backend.managers import BookManager, GameManager, SessionManager
 
 
@@ -27,7 +28,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # TODO: Fix this.
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -40,6 +41,11 @@ async def validated_game_name(game_name: str) -> str:
         raise HTTPException(status_code=400, detail=f"Game name {game_name} is not valid")
     return game_name
 
+def get_book_manager() -> BookManager:
+    """
+    Dependency to get the book manager.
+    """
+    return app.state.book_manager
 
 # -------------------------------------
 # REST API
@@ -52,23 +58,23 @@ async def root() -> models.SimpleResponse:
 
 
 @app.post("/new_game/tictactoe")
-async def new_tic_tac_toe_game() -> models.SimpleResponse:
-    new_game_id = app.state.book_manager.get_free_game_id()
+async def new_tic_tac_toe_game(book_manager: Annotated[BookManager, Depends(get_book_manager)]) -> models.SimpleResponse:
+    new_game_id = book_manager.get_free_game_id()
     game = TicTacToeGame()
     session = SessionManager(game.get_max_players())
     game_manager = GameManager(new_game_id, game, session)
-    app.state.book_manager.add_game(new_game_id, game_manager)
+    book_manager.add_game(new_game_id, game_manager)
     logger.info(f"New game of Tic Tac Toe created: {new_game_id}")
     return models.SimpleResponse(parameters=models.SimpleResponseParameters(message=new_game_id))
 
 
 @app.post("/new_game/ultimate")
-async def new_game() -> models.SimpleResponse:
-    new_game_id = app.state.book_manager.get_free_game_id()
+async def new_ultimate_game(book_manager: Annotated[BookManager, Depends(get_book_manager)]) -> models.SimpleResponse:
+    new_game_id = book_manager.get_free_game_id()
     game = UltimateGame()
     session = SessionManager(game.get_max_players())
     game_manager = GameManager(new_game_id, game, session)
-    app.state.book_manager.add_game(new_game_id, game_manager)
+    book_manager.add_game(new_game_id, game_manager)
     logger.info(f"New game of Ultimate Tic Tac Toe created: {new_game_id}")
     return models.SimpleResponse(parameters=models.SimpleResponseParameters(message=new_game_id))
 
@@ -76,12 +82,13 @@ async def new_game() -> models.SimpleResponse:
 @app.get("/game/{game_name}")
 async def get_game_metadata(
     game_name: Annotated[str, Depends(validated_game_name)],
+    book_manager: Annotated[BookManager, Depends(get_book_manager)],
 ) -> models.GameMetadata:
     """
     Get the game state for a given game name.
     """
     try:
-        metadata = app.state.book_manager.get_game_metadata(game_name)
+        metadata = book_manager.get_game_metadata(game_name)
     except ValueError:
         logger.info(f"Game {game_name} not found.")
         raise HTTPException(status_code=404, detail=f"Game {game_name} not found")
@@ -96,7 +103,9 @@ async def get_game_metadata(
 
 @app.websocket("/game/{game_name}/ws")
 async def websocket_endpoint(
-    game_name: Annotated[str, Depends(validated_game_name)], client_websocket: WebSocket
+    game_name: Annotated[str, Depends(validated_game_name)],
+    client_websocket: WebSocket,
+    book_manager: Annotated[BookManager, Depends(get_book_manager)],
 ):
-    game = app.state.book_manager.get_game(game_name)
+    game = book_manager.get_game(game_name)
     await game.handle_connection(client_websocket)
